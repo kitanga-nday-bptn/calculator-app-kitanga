df6312106ce6bf2549f36453402b9e768ca678fe
kitanga-nday-bptn <kitanga.zm@bptn.com>
2022-08-18 23:46:06 +0200

    Used to change background to correct colour based on theme

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,20 @@
+import { useTheme } from "@mui/material";
+import styled from "styled-components";
+import { useColorMode } from "../hooks/useColorMode";
+
+export const Wrapper = ({ children }: any) => {
+    const theme = useTheme()
+    const [mode] = useColorMode();
+
+    const Wrapper = styled.div`
+        background-color: ${theme.palette.background.default};
+        width: 100%;
+        height: 100%;
+    `;
+
+    return (
+        <Wrapper>
+            {children}
+        </Wrapper>
+    );
+}
