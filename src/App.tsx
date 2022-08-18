69348a300839c55a7f07a0b42d20a3b2e793d286
kitanga-nday-bptn <kitanga.zm@bptn.com>
2022-08-18 23:40:02 +0200

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+import { Calculator } from './pages/Home';
+import { NotFound } from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={(
+          <Calculator />
+        )} />
+        <Route path="*" element={(
+          <NotFound />
+        )} />
+      </Routes>
+      {/* <div className="App">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </div> */}
+    </Router>
   );
 }
 
