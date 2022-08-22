30295ef973c0eac79464244d6722599a3f261f15
kitanga-nday-bptn <kitanga.zm@bptn.com>
2022-08-22 14:59:42 +0200

    Responsiveness

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -8,8 +8,13 @@ export const Wrapper = ({ children }: any) => {
 
     const Wrapper = styled.div`
         background-color: ${theme.palette.background.default};
+        background-image: linear-gradient(304deg, rgba(57,242,250,1) 0%, rgba(57,242,250,1) 14%, rgba(80,255,251,1) 14%, rgba(80,255,251,1) 86%, rgba(57,242,250,1) 86%, rgba(57,242,250,1) 100%);
         width: 100%;
         height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-width: 300px;
     `;
 
     return (
